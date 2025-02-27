8e09985e975c3277673d0d9b698efdd819b7504c
Konstantinos Kefalas <konkef@kinsen.gr>
2025-02-27 15:48:11 +0200

    Added my project files

---
+++ src/components/Player/Player.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+export default function Player({initialName, symbol, isActive, onChangeName}){
+
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing , setIsEditing] = useState(false);
+
+    function handleEditClick(){
+        //update the state based on the previous value of the state
+        //We should pass a function! This is a strong recommendation of the React!
+        setIsEditing((editing)=> !editing);
+        onChangeName(symbol,playerName);
+    }
+
+    function handleChange(e){
+        setPlayerName(e.target.value);
+    }
+
+    let spanName = <span className="player-name">{playerName}</span>;
+
+    if (isEditing){
+        //we use value = {name} to pre polulate the value of the input to name of the current player after  the state change
+        spanName = <input type="text"  required  value = {playerName} onChange={handleChange}/>
+        
+    }
+
+    return (
+        <li className={isActive ? "active" : undefined}>
+              <span className="player">
+                {spanName}
+                <span className="player-symbol">{symbol}</span>
+              </span>
+              <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+            </li>
+    );
+} 
